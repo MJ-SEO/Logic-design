2aefdbd1764ecd3af0ebc94b385bfd95b40da7c0
MJ-SEO <21600343@handong.edu>
2020-05-17 21:41:40 +0900

    hard...

--- t.c
+++ t.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
+typedef struct{
+	int binary[200];
+	int check;
+}min;
+
 int main(){
-	int var;
+	int var=0, minterm=0, dc=0;
+	int arr[100];
 
-	printf("변수의 개수 입력: ");
+	printf("변수의 개수 입력: \n");
 	scanf("%d", &var);
-
-	printf("");
 	
+	printf("minterm의개수 입력: \n");
+	scanf("%d", &minterm);
+
+	printf("don't care조건의 개수 입력: \n");
+	scanf("%d", &dc);
+
+	printf("minterm 번호 입력: \n");
+	scanf("");
+
+	printf("00000? %d", arr[0]);
+
+		
+
 	return 0;
 }
